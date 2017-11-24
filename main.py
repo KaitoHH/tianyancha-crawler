287f85e465f6cd6cefaaa8dcabafb0a144d7120c
KaitoHH <hh.kaito@gmail.com>
2017-11-24 15:51:29 +0800

    io for csv file

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+import csv
 import io
 import re
 import utils
@@ -8,13 +9,18 @@ def getCompanyInfo(url):
     print(url)
     browser.get(url)
     company_name = browser.find_element_by_xpath(
-        '//*[@id="company_web_top"]/div[2]/div[2]/div/span[1]')
-    print(company_name.text)
-    element = browser.find_element_by_xpath(
-        '//*[@id="_container_holder"]/div/table/tbody')
+        '//*[@id="company_web_top"]/div[2]/div[2]/div/span[1]').text
+    try:
+        element = browser.find_element_by_xpath(
+            '//*[@id="_container_holder"]/div/table/tbody')
+    except:
+        return
     spt = element.text.split('\n')
-    groups = [spt[x:x + 1] + spt[x + 2:x + 4] for x in range(0, len(spt), 4)]
-    print(*groups, sep='\n')
+    groups = [[company_name] + spt[x:x + 1] + spt[x + 2:x + 4]
+              for x in range(0, len(spt), 4)]
+    with open('info.txt', 'a', encoding='utf-8') as csvfile:
+        writer = csv.writer(csvfile, delimiter='|')
+        writer.writerows(groups)
 
 
 f = open('company_list/company_01.txt', 'r')
@@ -25,5 +31,3 @@ utils.auto_login(browser)
 
 for line in f.readlines():
     getCompanyInfo(line.strip())
-    # with open('info.txt', 'a', encoding='utf-8') as file:
-    #     file.write('\n'.join(result))
