efc46a809e3cc83f2d0b9926167f991bc60d10ea
KaitoHH <hh.kaito@gmail.com>
2017-11-25 14:05:15 +0800

    switched to config.py

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import time
 import json
+from config import config
 
 def wait_for_login(browser):
     browser.get('https://www.tianyancha.com/login')
@@ -13,10 +14,8 @@ def auto_login(browser):
         '//*[@id="web-content"]/div/div/div/div[2]/div/div[2]/div[2]/div[2]/div[2]/input')
     pwd = browser.find_element_by_xpath(
         '//*[@id="web-content"]/div/div/div/div[2]/div/div[2]/div[2]/div[2]/div[3]/input')
-    with open('config.json') as f:
-        config = json.load(f)
-        user.send_keys(config['username'])
-        pwd.send_keys(config['password'])
+    user.send_keys(config['username'])
+    pwd.send_keys(config['password'])
     browser.find_element_by_xpath(
         '//*[@id="web-content"]/div/div/div/div[2]/div/div[2]/div[2]/div[2]/div[5]').click()
     time.sleep(1)
