cd8655bc4289638086c5572839e4c1d36b61ec16
KaitoHH <hh.kaito@gmail.com>
2017-11-26 12:10:32 +0800

    opt PIL

--- image.py
+++ image.py
@@ -2,7 +2,12 @@ from PIL import Image
 
 
 def merge_captcha():
-    newImage = Image.new('RGBA', (320, 130))
+    newImage = Image.new('RGBA', (320, 160))
     newImage.paste(Image.open('B.png'), (0, 0))
-    newImage.paste(Image.open('A.png'), (0, 100))
+    newImage.paste(Image.open('hint.png'), (0, 100))
+    newImage.paste(Image.open('A.png'), (0, 130))
     newImage.save('C.png')
+
+
+if __name__ == '__main__':
+    merge_captcha()
